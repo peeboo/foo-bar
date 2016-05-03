054eb6739dd1b2d9c33c201b056ae01128cb06b3
peeboo <ozon752@gmail.com>
2016-05-03 15:36:18 +0200

    start

---
+++ bar/foo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace foo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello world");
+            Console.WriteLine();
+            Console.Write("Square(8) = {0}", Square(8));
+        }
+
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern int Square(int number);
+    }
+}
